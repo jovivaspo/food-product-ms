c19c111d0556587c12c4a4c21ce5fea32bb43259
jovivaspo <jorgevipo.dev@gmail.com>
2025-05-14 10:52:35 +0200

    feat: delete test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [CategoryModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
